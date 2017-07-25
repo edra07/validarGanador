18ebdf1dd53864853c7ee478e0b01d2bd35cb440
Edin <22ramirez.edin@gmail.com>
2017-07-24 23:35:27 -0500

    atributos de los objetos de las imagenes

---
+++ Pagina/Js/ObjetosImagenes.js
@@ -0,0 +1,60 @@
+var imagen1 = {
+	 "id": "35895541381", "owner": "7800715@N08", "secret": "338fec5630", "server": "4306", "farm": 5, "title": "Seaport timelaps", "ispublic": 1, "isfriend": 0, "isfamily": 0 
+}
+var imagen2 = {
+"id": "35639849520", "owner": "57090561@N08", "secret": "24d294453b", "server": "4292", "farm": 5, "title": "Signs", "ispublic": 1, "isfriend": 0, "isfamily": 0 
+}
+var imagen3 = {
+"id": "35987717306", "owner": "151970786@N07", "secret": "c05927a597", "server": "4314", "farm": 5, "title": "Smoke and breeze", "ispublic": 1, "isfriend": 0, "isfamily": 0 
+}
+var imagen4 = {
+"id": "35639743390", "owner": "150648255@N03", "secret": "500ac6984e", "server": "4312", "farm": 5, "title": "Karlovy Vary from bird's eye-view", "ispublic": 1, "isfriend": 0, "isfamily": 0
+}
+var imagen5 = {
+"id": "35639743390", "owner": "150648255@N03", "secret": "500ac6984e", "server": "4312", "farm": 5, "title": "Karlovy Vary from bird's eye-view", "ispublic": 1, "isfriend": 0, "isfamily": 0
+}
+var imagen6 = {
+"id": "35189470664", "owner": "27675566@N08", "secret": "34fc85def7", "server": "4295", "farm": 5, "title": "Richard Mrazek CFAME 1979 YAK 10 T performing at the 2017 Arlington Fly-In", "ispublic": 1, "isfriend": 0, "isfamily": 0 
+}
+var imagen7 = {
+"id": "35639637660", "owner": "27675566@N08", "secret": "10edf14430", "server": "4330", "farm": 5, "title": "Richard Mrazek C-FAME 1979 YAK 10 T performing at the 2017 Arlington Fly-In", "ispublic": 1, "isfriend": 0, "isfamily": 0
+}
+var imagen8 = {
+"id": "35218744843", "owner": "27675566@N08", "secret": "3b5332d520", "server": "4298", "farm": 5, "title": "Richard Mrazek C-FAME 1979 YAK 10 T performing at the 2017 Arlington Fly-In", "ispublic": 1, "isfriend": 0, "isfamily": 0
+}
+var imagen9 = {
+"id": "35987558916", "owner": "27675566@N08", "secret": "ffa0fcaf56", "server": "4309", "farm": 5, "title": "Richard Mrazek CFAME 1979 YAK 10 T performing at the 2017 Arlington Fly-In", "ispublic": 1, "isfriend": 0, "isfamily": 0 
+}
+var imagen10 = {
+"id": "35189464534", "owner": "27675566@N08", "secret": "7c51cfa13d", "server": "4299", "farm": 5, "title": "Richard Mrazek C-FAME 1979 YAK 10 T performing at the 2017 Arlington Fly-In", "ispublic": 1, "isfriend": 0, "isfamily": 0 
+}
+var imagen11 = {
+"id": "35218741053", "owner": "27675566@N08", "secret": "610d37acbd", "server": "4294", "farm": 5, "title": "Richard Mrazek C-FAME 1979 YAK 10 T performing at the 2017 Arlington Fly-In", "ispublic": 1, "isfriend": 0, "isfamily": 0 
+}
+var imagen12 = {
+"id": "35639632890", "owner": "27675566@N08", "secret": "c295e5bda9", "server": "4292", "farm": 5, "title": "Richard Mrazek CFAME 1979 YAK 10 T performing at the 2017 Arlington Fly-In", "ispublic": 1, "isfriend": 0, "isfamily": 0
+}
+var imagen13 = {
+ "id": "35218788263", "owner": "104000687@N08", "secret": "f6cd083885", "server": "4324", "farm": 5, "title": "Outhgill near Kirkby Stephen 14 June 2017-0010-Edit.jpg", "ispublic": 1, "isfriend": 0, "isfamily": 0
+}
+var imagen14 = {
+ "id": "36029159395", "owner": "150648255@N03", "secret": "b7027c30a1", "server": "4311", "farm": 5, "title": "Streets of Karlovy Vary [panorama]", "ispublic": 1, "isfriend": 0, "isfamily": 0
+}
+var imagen15 = {
+ "id": "35218699143", "owner": "53317326@N02", "secret": "4e6526a4bb", "server": "4315", "farm": 5, "title": "#lavender #flower #photography #instagram @photobypixie", "ispublic": 1, "isfriend": 0, "isfamily": 0 
+}
+var imagen16 = {
+ "id": "35895264731", "owner": "26583258@N08", "secret": "1dce19dcb8", "server": "4310", "farm": 5, "title": "Charles #nantes #Hipstamatic #photography #instagood #picoftheday #hipsta_crazy #hipstaoftheday #photooftheday #iphoneography #mobile_phoneography #Instagram #photoweek #Shootermag_france #Streetphotography #art #France #bnw_demand #bnw #bnw_magazine", "ispublic": 1, "isfriend": 0, "isfamily": 0 
+}
+var imagen17 = {
+ "id": "35857950532", "owner": "94373667@N08", "secret": "7e0c49d823", "server": "4327", "farm": 5, "title": "╰☆╮Hold me close to your heart╰☆╮", "ispublic": 1, "isfriend": 0, "isfamily": 0
+}
+var imagen18 = {
+ "id": "35987451286", "owner": "29438012@N02", "secret": "cb91ccea97", "server": "4303", "farm": 5, "title": "02c140717", "ispublic": 1, "isfriend": 0, "isfamily": 0 
+}
+var imagen19 = {
+"id": "35857942102", "owner": "29438012@N02", "secret": "54102a8214", "server": "4329", "farm": 5, "title": "01c140717", "ispublic": 1, "isfriend": 0, "isfamily": 0 
+}
+var imagen20 = {
+"id": "35895444381", "owner": "14533495@N00", "secret": "e7880d0696", "server": "4296", "farm": 5, "title": "DKH_6331", "ispublic": 1, "isfriend": 0, "isfamily": 0 
+}
